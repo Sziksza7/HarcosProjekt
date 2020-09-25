b47e02bc2a40ea79f0f699d137c2c678161fee29
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:05:25 +0200

    Class, és adattagok létrehozása

---
+++ HarcosProjekt/Harcos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+    }
+}
