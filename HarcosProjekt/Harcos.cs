bb89d2d91c3ccb4f8f33468a2a1cb1a8e9ed8e9c
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:07:09 +0200

    Getter, setter

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -15,12 +15,19 @@ namespace HarcosProjekt
 
         public Harcos(string nev, int szint, int tapasztalat, int eletero, int alapEletero, int alapSebzes)
         {
-            this.nev = nev;
-            this.szint = szint;
-            this.tapasztalat = tapasztalat;
-            this.eletero = eletero;
-            this.alapEletero = alapEletero;
-            this.alapSebzes = alapSebzes;
+            this.Nev = nev;
+            this.Szint = szint;
+            this.Tapasztalat = tapasztalat;
+            this.Eletero = eletero;
+            this.AlapEletero = alapEletero;
+            this.AlapSebzes = alapSebzes;
         }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
+        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
     }
 }
