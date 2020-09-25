ad359f31e184d5f16cca3886deb8b2ab78116219
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 10:34:10 +0200

    Átalakítás, próba a hibák kijavítására

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -13,24 +13,30 @@ namespace HarcosProjekt
         private int alapEletero;
         private int alapSebzes;
 
-        public Harcos(string nev, int szint, int tapasztalat, int eletero, int alapEletero, int alapSebzes)
+        public Harcos(string nev, int statuszSablon)
         {
             this.Nev = nev;
-            this.Szint = szint;
-            this.Tapasztalat = tapasztalat;
-            this.Eletero = eletero;
-            this.AlapEletero = alapEletero;
-            this.AlapSebzes = alapSebzes;
+            this.Szint = 1;
+            this.Tapasztalat = 0;
+            this.Eletero = MaxEletero;
+            if (statuszSablon == 1) { this.AlapEletero = 15; this.AlapSebzes = 3; }
+            else if (statuszSablon == 2) { this.AlapEletero = 12; this.AlapSebzes = 4; }
+            else if (statuszSablon == 3) { this.AlapEletero = 8; this.AlapSebzes = 5; }
         }
 
         public string Nev { get => nev; set => nev = value; }
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
         public int Eletero { get => eletero; set => eletero = value; }
-        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
-        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
-        public int Sebzes { get => Sebzes; set => Sebzes = alapSebzes*szint; }
-        public int SzintLepeshez{ get => SzintLepeshez; set => SzintLepeshez = 10+(szint*5); }
-        public int MaxEletero{ get => MaxEletero; set => MaxEletero = AlapEletero+(szint*3); }
+        public int AlapEletero { get => alapEletero; }
+        public int AlapSebzes { get => alapSebzes; }
+        public int Sebzes { get => alapSebzes+szint; }
+        public int SzintLepeshez{ get => 10+szint*5; }
+        public int MaxEletero{ get => alapEletero+szint*3;  }
+
+        public void ToString() { }
+        public void Gyogyul() { }
+        public void Megkuzd() { }
+        
     }
 }
